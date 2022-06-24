c900d74e9723b753ef53a15828e588940fa8a7f2
MartinS <martin.sedlak777@gmail.com>
2022-06-24 12:21:04 +0200

    Create parser file that contains all needed logic for cleaning up a repo
    + Copy + paste root of the script from Matous it contains basic logic for parsing xml, get image names of used images and all images, print and seperate by color used and unsuded
    + I seperate logic for images and exercises by comments into the block
    + Added delete function for unused images
    + Created whole new block to handle exercises operation
    + Added get_exercise_name_and_path() func that create dict of used exer.  with keys as exer. names and values as exer. paths
    + Added filter_the_exercise_files() func that create dict of all(used/unused) exer.  with keys as exer. names and values as exer. paths
    + Added delete_unused_exercises() func that delete all unused exer. It deletes the whole exer. folder

---
+++ parser.py
@@ -0,0 +1,148 @@
+import os
+import json
+from shutil import rmtree
+import xml.etree.ElementTree
+
+from termcolor import colored
+
+
+def main_parser(xml_file, option: str, specified_file: str):
+    """
+    Parse the given XML file and find all the tags with attribute TYPE (value:
+    image).
+    """
+    root: xml.etree.ElementTree.Element = parse_xml_file(xml_file)
+
+    # Specific for handling images
+    images: tuple = tuple(get_all_images(root, "widget"))
+    image_names: tuple = tuple(get_image_name(images))
+    all_image_files: tuple = tuple(filter_the_image_files(os.listdir()))
+    #delete_unused_images(image_names, all_image_files)
+
+    # Specific for handling exercises
+    exercise_names_and_paths = get_exercise_name_and_path(root, "solution")
+    all_exercise_folders = filter_the_exercise_files()
+    #delete_unused_exercises(exercises, all_exercise_folders)
+
+    #make_suggestions(exercise_names, all_exercise_folders)
+    
+
+def parse_xml_file(xml_file: str) -> xml.etree.ElementTree.Element:
+    """
+    Parse the given xml file - xml file all needed information about academy
+    """
+    tree = xml.etree.ElementTree.parse(xml_file)
+    return tree.getroot()
+
+
+# <---- Function needed for handling IMAGES ----> START
+
+def get_all_images(root: xml.etree.ElementTree.Element, tagname: str) -> list:
+    """
+    Iterate through all tags with the given TAGNAME and attribute TYPE="image".
+    """
+    return [
+        xml_tag
+        for xml_tag in root.iter(tagname)
+        if xml_tag.get("type") == "image"
+    ]
+
+
+def get_image_name(elements: tuple) -> list:
+    """
+    Return only the image names.
+    """
+    return [
+        json.loads(image_el.text.strip()).get("src")
+        for image_el in elements
+    ]
+
+
+def filter_the_image_files(all_files: list) -> list:
+    """
+    Return only the JPG and PNG files.
+    """
+    return [
+        file
+        for file in all_files
+        if file.endswith(".png") or file.endswith(".jpg")
+    ]
+
+def delete_unused_images(images: tuple, filetree: tuple):
+    """
+    If image is not used(is in file tree but isn't in list of used images) delete the image.
+    """
+    for image in filetree: 
+        if image not in images:
+            os.remove(image)
+
+    # ?It's necessary to have a return here? If yes how to build a function properly
+
+# <---- Function needed for handling IMAGES ----> END
+
+
+# <---- Function needed for handling EXERCISES ----> START
+
+def get_exercise_name_and_path(root: xml.etree.ElementTree.Element, tagname: str) -> dict:
+    """
+    Return dict with keys as exercise names and values as exercise paths.
+    Contains only exercises that are in use. 
+    Key:value pair is needed for delete function 
+    """
+    exercise_names_paths: dict = {}
+
+    for xml_tag in root.iter(tagname):
+        exercise_name = xml_tag.get("sourceDir").split("/", 3)[2]
+        exercise_path = xml_tag.get("sourceDir").replace("/solution", '')
+        exercise_names_paths[exercise_name] = exercise_path
+
+    return exercise_names_paths
+
+
+def filter_the_exercise_files() -> dict:
+    """
+    Return dict with keys as exercise names and values as exercise paths.
+    Contains all exercises.
+    Key:value pair is needed for delete function 
+    """
+    all_exercise_names_and_paths: dict = {}
+
+    path_to_exercises_dir = os.getcwd() + "/exercises"
+    
+    for lesson_dir in os.listdir(path_to_exercises_dir):
+        lesson_paths = os.path.join(path_to_exercises_dir, lesson_dir)
+        for exercise_folder in os.listdir(lesson_paths):
+            # Relative path looks like = exercises/L05/count_numbers
+            exercise_path = f"{lesson_paths.split('/', 6)[6]}/{exercise_folder}"
+            all_exercise_names_and_paths[exercise_folder] = exercise_path
+    
+    return all_exercise_names_and_paths
+
+
+def delete_unused_exercises(in_use_exercises: tuple, all_exercises: tuple):
+    """
+    If exercise is not used(is in file tree of all exercises but isn't in list of used exercises) delete the exercise recursively.
+    Delete whole folder with everything in it 
+    """
+    for folder in all_exercises:
+        if folder not in in_use_exercises.keys():
+            rmtree(all_exercises[folder])
+
+    # ?It's necessary to have a return here? If yes how to build a function properly
+
+# <---- Function needed for handling EXERCISES ----> END
+
+
+def make_suggestions(used_files: tuple, filetree: tuple) -> None:
+    """
+    Write sys.stdout if the file exists in the current dir.
+    Function works on images and on exercises as well depends on given arguments
+    """
+    for file in filetree:
+        if file in used_files:
+            print(colored(f"CORRECT: '{file}'..", "green"))
+        else:
+            print(colored(f"REMOVING: '{file}'..", "red"))
+
+
+
